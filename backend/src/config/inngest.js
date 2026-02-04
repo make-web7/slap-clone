870e6d60129772d9e391cc9507ba988e91dd4a9e
make-web7 <aarav.raghavan@outlook.com>
2026-02-03 22:21:32 -0500

    Debugging

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,6 @@
 import { Inngest } from "inngest";
 import {connectDB} from "./db.js";
-
+import {User} from "../models/user.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slap-clone" });
 
@@ -24,6 +24,7 @@ const deleteUserFromDB = inngest.createFunction(
     {id: "delete-user-from-db",},
     {event:"clerk/user.deleted"},
     async ({event}) => {
+        await connectDB()
         const {id} = event.data;
         await User.deleteOne({clerkId: id})
     }
