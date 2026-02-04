e1996bccefca82e12e1a1f962e6cc0b89e4f25d8
make-web7 <aarav.raghavan@outlook.com>
2026-02-03 21:21:49 -0500

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
-import express from 'express';
 import {ENV} from "./config/env.js"
 import {connectDB} from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import express from "express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./config/inngest.js"
+
 const app = express();
 
+app.use(clerkMiddleware());
+
+app.use(express.json());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -10,9 +18,20 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(ENV.PORT, () => {
-    console.log('Server running on port: ' + ENV.PORT);
-    connectDB()
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+        if (ENV.NODE_ENV !== "production") {
+            app.liste(ENV.PORT, () => {
+                console.log(`Server started on port ${ENV.PORT}`);
+            })
+        }
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+
 
 // aaravraghavan_db_user: rzjEmHqVmhQbhi8i
