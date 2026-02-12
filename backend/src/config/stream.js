af2f23e8f46f6322e5e7b29931a6ba862903e49e
make-web7 <aarav.raghavan@outlook.com>
2026-02-12 15:14:32 -0500

    Stream & Sentry added

---
+++ backend/src/config/stream.js
@@ -0,0 +1,32 @@
+import {StreamChat} from "stream-chat"
+import {ENV} from "../config/env.js"
+
+const streamClient = StreamChat.getInstance(ENV.STREAM_API_KEY, ENV.STREAM_API_SECRET)
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        await streamClient.upsertUser(userData)
+        console.log('Upsertedstream user successfully', userData.name)
+    } catch (error) {
+        console.error("error deleting stream user", error)
+    }
+}
+
+export const deleteStreamUser = async (userId) => {
+    try {
+        await streamClient.deleteUser(userId)
+        console.log(' user deleted successfully', userData.name)
+    } catch (error) {
+        console.error("error deleting stream user", error)
+    }
+}
+
+export const generateStreamToken = (userId) => {
+    try {
+        const userIdString = userId.toString();
+        return streamClient.createToken(userIdString)
+    } catch (error) {
+        console.error("error deleting stream token", error)
+        return null
+    }
+}
