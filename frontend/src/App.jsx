2ce15956e46b4be2be2010fd33425ce8853fac2b
make-web7 <aarav.raghavan@outlook.com>
2026-02-21 13:29:40 -0500

    useStreamChat custom hook added and routing updated.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,24 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import {SignedIn, SignedOut, SignInButton, useAuth, UserButton} from '@clerk/clerk-react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import AuthPage from "./pages/AuthPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import * as Sentry from "@sentry/react";
-
+import CallPage from "./pages/CallPage.jsx";
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes)
 function App() {
-    const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes)
-    return (
-        <>
+    const {isSignedIn, isLoaded} = useAuth()
 
-            <SignedIn>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/auth" element={<Navigate to={"/"} replace />} />
-                </Routes>
-            </SignedIn>
-            <SignedOut>
-
-
-                <Routes>
-                    <Route path="/auth" element={<AuthPage />} />
-                    <Route path="*" element={<Navigate to={"/auth"} replace />} />
-                </Routes>
-            </SignedOut>
-
-        </>
-    );
+    if(!isLoaded) {
+        return null
+    }
+        return (
+                <SentryRoutes>
+                    <Route path="/" element={isSignedIn ? <HomePage /> : <Navigate to={"/auth"} replace/>} />
+                    <Route path="/auth" element={!isSignedIn ? <AuthPage/> : <Navigate to={"/"} replace />} />
+                    <Route path="/call/:id" element={isSignedIn ? <CallPage /> : <Navigate to={"/auth"} replace/>} />
+                    <Route path="*" element={ isSignedIn ? <Navigate to={"/"} replace/> : <Navigate to={"/auth"} replace />} />
+                </SentryRoutes>
+);
 }
 
 export default App
